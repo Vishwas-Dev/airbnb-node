5330ee4d3802f8dc2fef7cca316cc139bad8ec97
Vishwas-Dev <vishwasgupta795@gmail.com>
2026-09-14 08:49:40 +0530

    updated err in migrations

--- src/db/migrations/20260909035801-add-ratings-hotel-table.ts
+++ src/db/migrations/20260909035801-add-ratings-hotel-table.ts
@@ -1,18 +1,20 @@
-import { queryInterface } from "sequelize";
+import { QueryInterface } from "sequelize";
 module.exports = {
-  async up (queryInterface : queryInterface) {
+  async up (queryInterface : QueryInterface) {
     await queryInterface.sequelize.query(`
       ALTER TABLE hotels
       ADD COLUMN rating DECIMAL(3,2) DEFAULT NULL,
       ADD COLUMN rating_count INT DEFUALT NULL
-    )
+    `)
   },
 
-  async down (queryInterface : queryInterface ) {
+  async down (queryInterface : QueryInterface ) {
     await queryInterface.sequelize.query(`
-      ALTER TABLE hotels
+      ALTER TABLE hotels,
       DROP COLUMN rating,
       DROP COLUMN rating_count
-    )
+      
+    `)
   }
 };
+
