7904943592c790a063d6b28bf64803f540732345
Vishwas-Dev <vishwasgupta795@gmail.com>
2026-09-21 08:25:10 +0530

    added business logic with exists gmail in practice api

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,8 @@
+import { logger } from "../config/logger.config.js";
 import User from "../db/modeles/user.js";
 import { CreateUserDTO } from "../dto/user.dto.js";
 import { createUser, getUserById } from "../repository/user.repository.js";
+import { NotFoundError } from "../utilis/error/app.error.js";
 
  export async function getUserByEmail(email: string) {
   return await User.findOne({
@@ -14,7 +16,8 @@ export async function createUserService(userData: CreateUserDTO){
     const existingUser = await getUserByEmail(userData.email);
 
   if (existingUser) {
-    throw new Error("User with this email already exists");
+        logger.error(`gmail is already exists ${userData.email}`);
+            throw new NotFoundError(`user with ${userData.email} already exists`); 
   }
 
     const user = await createUser(userData);
